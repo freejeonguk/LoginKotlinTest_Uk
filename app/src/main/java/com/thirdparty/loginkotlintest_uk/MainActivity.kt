f6bbc1ec93737ce8cf8ccba9384e27a0a9de8ba4
Uk <qmdlwd2@gmail.com>
2022-11-06 18:02:18 +0900

    코드에 적은 관리자 이름도 변수에 나오게 - String 가공하기

--- app/src/main/java/com/thirdparty/loginkotlintest_uk/MainActivity.kt
+++ app/src/main/java/com/thirdparty/loginkotlintest_uk/MainActivity.kt
@@ -2,10 +2,34 @@ package com.thirdparty.loginkotlintest_uk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginBtn.setOnClickListener {
+
+            val inputId = idEdt.text.toString()
+            val inputPw = passwordEdt.text.toString()
+
+
+            if (inputId == "admin@test.com" && inputPw == "qwer") {
+
+                val name = "욱이"
+
+                Toast.makeText(this, "${name} 관리자입니다.", Toast.LENGTH_SHORT).show()
+
+
+            } else {
+
+                Toast.makeText(this, "로그인 실패", Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+
     }
 }
